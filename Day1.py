60c809fd522654f4f6a6ca3759bf2a6445fc5510
Harshvardhan <121004255+Harshvardhanabc@users.noreply.github.com>
2024-02-10 18:26:43 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,7 @@
+# Write a program to print the given number is odd or even.
+number = int(input("Enter any integer value here = "))
+if(number%2 == 0):
+    print(number,"is even")
+
+else:
+    print(number,"is odd")
